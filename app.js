f85ad07299f3e3e0aac0455a367472acf1e17bf6
Vinski <vinski.andrade@visy.fi>
2024-11-11 14:19:32 -0300

    obj name corrected

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var healthCheckRouter = require('./routes/healthcheck');
+var healthcheckRouter = require('./routes/healthcheck');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/healthCheck', healthCheckRouter);
+app.use('/healthcheck', healthcheckRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
